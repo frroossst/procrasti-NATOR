172d4401734912b24a6e2aecb54755d270bb278e
frroossst <adhyanpatel@gmail.com>
2020-06-23 12:02:19 +0530

    2.1.1 new logs error added

--- procrast_mod.py
+++ procrast_mod.py
@@ -1,4 +1,4 @@
-#version 2.1.0
+#version 2.1.2
 import time
 import datetime
 import webbrowser
@@ -7,8 +7,9 @@ import codecs
 #filemode='w' for overwriting and erase for continued logs
 log_format = "%(asctime)s::%(levelname)s::%(name)s::"\
 "%(filename)s::%(lineno)d::%(message)s"
-logging.basicConfig(filename='/home/home/Desktop/flow_state/logfile.log' , level='DEBUG', format=log_format,filemode='w')
+logging.basicConfig(filename='/home/home/Desktop/flow_state/logfile.log' , level='DEBUG', format=log_format)
 print("procrasti-NATOR 2020")
+logging.debug("---START---")
 print("created by -frroossst 02:14 31 May 2020")
 time.sleep(1)
 print("Commands:")
@@ -30,8 +31,7 @@ time.sleep(0.1)
 print("/break - to take a break")
 time.sleep(0.5)
 now=datetime.datetime.now()
-dist_timings=[]
-break_timings=[]
+dist_timings=break_timings=[]
 avoid=pro=br=br_total=br_timez=pomo_count=0
 ch = '/begin'
 print("start time =",now)
@@ -40,12 +40,12 @@ while ch == '/begin' or ch == '/cont' or ch=='/avoid' or ch=='/break' or ch =='/
     pro_check = input()
     if pro_check == 'y' or pro_check == 'Y':
         pro += 1
-        logging.debug("Continued Distraction")
+        logging.debug("continued distraction")
         dist_timings.append(datetime.datetime.now())
         print("Total number of times you've been distracted this session :", pro)
         ch = input("command :")
         if ch == '/avoid':
-            logging.debug("Avoidable distraction")
+            logging.debug("avoidable distraction")
             avoid+=1
             print("Total number of avoidable distraction =",avoid,"of",pro)
         elif ch == '/man':
@@ -53,11 +53,12 @@ while ch == '/begin' or ch == '/cont' or ch=='/avoid' or ch=='/break' or ch =='/
             man=open("/home/home/Desktop/flow_state/manual.html","r")
             webbrowser.open_new_tab('manual.html')
         elif ch == '/break':
-            logging.debug("Break")
+            logging.debug("break")
             break_timings.append(datetime.datetime.now())
             pro-=1
             br_time=int(input("Enter break duration (in minutes) :"))
             if br_time<0:
+                logging.error("0.0.2")
                 print("UNDERFLOWED TIMINGS")
                 print("ERROR CODE : 0.0.2 | Refer the manual for debug")
                 break    
@@ -67,10 +68,6 @@ while ch == '/begin' or ch == '/cont' or ch=='/avoid' or ch=='/break' or ch =='/
                 br+=1
                 print("total number of breaks =",br)
                 br_total+=br_time 
-        '''elif ch=='/log':
-            logging.debug("Opened log")
-            with codecs.open("/home/home/Desktop/flow_state/logfile.log",encoding="utf8") as infile:
-                lines=infile.readlines()'''
         elif ch == '/pomodoro':
             logging.debug("pomodoro init")
             pro-=1
@@ -84,11 +81,12 @@ while ch == '/begin' or ch == '/cont' or ch=='/avoid' or ch=='/break' or ch =='/
                 print("pomodoro sessions completed =",pomo_count)
                 print("you deserve a break")            
     elif pro==0 and avoid==0:
+        logging.error("0.0.1")
         print("NULL ERROR")
         print("ERROR CODE : 0.0.1 | Refer the manual for debug")
         break
     else:
-        logging.debug("program end")
+        logging.debug("---END---")
         print('total distractions =', pro)
         print('avoidable distractions =',avoid)
         print('unavoidable distractions =',pro-avoid)
